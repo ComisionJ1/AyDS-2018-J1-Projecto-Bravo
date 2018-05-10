69dc37059a4ae21be361ddbd7a61782f27262406
Tomas Perotti <tomas.perotti@outlook.com>
2018-05-10 15:07:20 -0300

    -Implementacion de errores terminada

--- JDictionaryBravo/src/main/java/ayds/dictionary/bravo/Model/Exception/UnallowedCharacterException.java
+++
@@ -1,8 +0,0 @@
-package ayds.dictionary.bravo.Model.Exception;
-
-public class UnallowedCharacterException extends Exception {
-
-    public String getMessage(){
-        return "Ha ingresado un caracter prohibido. Debe ingresar caracteres de a-Z.";
-    }
-}
